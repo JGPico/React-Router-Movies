b922482e098af500a22fac8788ab5f12692652ee
JGPico <minq_2@hotmail.com>
2020-02-25 15:11:35 -0700

    Working on dependency array

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,9 +15,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route path='/'>Home</Route>
-      <Route path='/movies:id'>Movies</Route>
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/'>
+        <MovieList/>
+      </Route>
+      <Route path='/movies:id'>
+        <Movie/>
+      </Route>
     </div>
   );
 };
